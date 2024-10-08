0bf96e75f9f5dc732fb4071a7868890b9e8d496f
NguyenDaiSon <daisonit@gmail.com>
2024-10-08 21:44:22 +0800

    Add files via upload

---
+++ practice_bokken_daa4a53f.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // borrow self immutably
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let area = rect1.area();
+    println!("The area of the rectangle is {area} square pixels.");
+
+    let rect2 = Rectangle::square(20);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect1? {}", rect2.can_hold(&rect1));
+}
